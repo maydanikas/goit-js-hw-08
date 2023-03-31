2961b7d5231918e1fe1e9ecfff08d00537c2a352
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-03-31 17:06:00 +0200

    task 2 vimeo player done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(data);
+}
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime || 0);
+player.on('timeupdate', throttle(onPlay, 1000));
